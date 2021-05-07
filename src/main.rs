ff16e5e95ad0ee5c1a3f7dddd1dbf21714bbaa43
Remy Wang <remywang@cs.washington.edu>
2021-05-07 11:19:57 -0700

    cleanup.

--- src/main.rs
+++ src/main.rs
@@ -15,30 +15,5 @@ fn main() {
     Runner::default()
         .with_expr(&"a".parse().unwrap())
         .run(&axioms())
-        .with_hook(|_| {
-            println!("iter");
-            Ok(())
-        })
-        .print_report()
+        .print_report();
 }
-
-// fn main() {
-//     let a = "a".parse().unwrap();
-//     let mut i = 0;
-//     Runner::default()
-//         .with_expr(&a)
-//         .with_hook(move |runner| {
-//             i += 1;
-//             runner.egraph
-//                   .dot()
-//                   .to_png(format!("{}.png", i))
-//                   .unwrap();
-//             Ok(())
-//         })
-//         .run(&axioms())
-//         .egraph
-//         .dot()
-//         .to_png("end.png")
-//         .unwrap();
-// }
-
